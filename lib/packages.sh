624ad6e87959dd87b99dc0320dffcf2f9832bc75
zerolinux-os <zerolinuxproject@gmail.com>
2026-03-22 17:04:11 +0100

    fix: paru as user + remove calamares from list

--- lib/packages.sh
+++ lib/packages.sh
@@ -6,20 +6,17 @@ PACKAGES_URL="https://raw.githubusercontent.com/zerolinux-os/zerolinux-packages/
 install_packages() {
     step "Installing ZeroLinux packages"
 
-    # جلب قائمة الحزم
     info "Fetching packages list..."
     local tmpfile=$(mktemp)
     curl -sL "$PACKAGES_URL" -o "$tmpfile" || error "Failed to fetch packages list!"
 
-    # تصفية التعليقات والأسطر الفارغة
     local packages=$(grep -v "^#" "$tmpfile" | grep -v "^$" | sed "s/[[:space:]]//g" | tr "\n" " ")
     rm -f "$tmpfile"
 
-    # تثبيت الحزم
     info "Installing packages (this may take a while)..."
     if ! pacman -S --noconfirm --needed $packages 2>/dev/null; then
         warning "Some packages failed, trying with paru..."
-        paru -S --noconfirm --needed $packages
+        sudo -u "$REAL_USER" paru -S --noconfirm --needed $packages
     fi
     success "Packages installed successfully"
 }
@@ -42,7 +39,7 @@ install_zerolinux_packages() {
         git clone --depth=1 "https://github.com/zerolinux-os/zerolinux-packages.git" \
             "$tmpdir/repo" &>/dev/null
         cd "$tmpdir/repo/$pkg"
-        sudo -u "$REAL_USER" makepkg -si --noconfirm &>/dev/null && \
+        sudo -u "$REAL_USER" makepkg -si --noconfirm 2>/dev/null && \
             success "$pkg installed" || warning "$pkg failed"
         cd ~
         rm -rf "$tmpdir"
